8ac85dfa4be7a531aeca411fa64c1337b58b159f
Shaon6767 <chowdhuryshaon70@gmail.com>
2026-09-14 10:55:57 +0600

    operators

---
+++ JS/operators.js
@@ -0,0 +1,62 @@
+// let a = 3;
+// let b = ++a;
+// console.log(b);
+
+// let a = 3;
+// let b = a++;
+// console.log(b);
+
+// let a = 5;
+// let b = a++;
+// console.log(b);
+// console.log(a)
+
+// let counter = 0;
+// let c = counter++;
+// console.log(counter);
+// console.log(c);
+
+// let counter = 2;
+// let b = 2 * counter++; //counter = 2
+// console.log(b); //4
+// console.log(counter); //3
+// let c = 2 * ++counter; //counter = 4
+// console.log(c); //8
+// console.log(counter); // counter = 5??
+
+// for (a = 3, b = 2, c = a * b; c < 10; c++) {
+//   console.log(c) // 6 7 8 9
+// }
+
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(a, x); // 4 5
+
+// console.log(null);
+//  let v = typeof null
+//  console.log(v)
+
+// <----Logical Operators--->
+
+// let hour = 12;
+
+// if (hour > 10 || hour < 10) {
+//   console.log("hour is less than 10");
+// } else {
+//   console.log("hour remains");
+// }
+
+// console.log(1 && 2 && null && 0 && 3); // null
+// console.log(1 && undefined && 0); // undefined
+// console.log(1 && 0); // 0
+// console.log(1 && 1 && 0); // 0
+
+// let hour = 20;
+
+// if (hour > 10) {
+//   console.log("hour is gone");
+// } else if (hour === 10 || (hour && 10) || hour < 10) {
+//   console.log("hour remains");
+// }
+
+// console.log("hour" && 10);
