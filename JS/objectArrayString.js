14f044a1e4654d49e92559658281bc59203de329
Shaon6767 <chowdhuryshaon70@gmail.com>
2026-09-14 22:46:21 +0600

    objects

---
+++ JS/objectArrayString.js
@@ -0,0 +1,51 @@
+// let obj = new Object(); // constructor method
+// console.log(obj)
+
+// let obj = {} // object literal
+
+// let obj = {
+//   name: "helo",
+//   age: "69",
+// };
+// console.log(obj);
+
+// let obj1 = {
+//   name: "hello",
+//   age: "690",
+//   "is dying": true,
+// };
+// console.log(obj1);
+
+// let obj = {
+//   name: "nice",
+//   age: "200",
+//   "likes tea": true,
+// };
+// console.log("name" in obj);
+// console.log("likes tea" in obj);
+
+// let user = {
+//   name: "John",
+//   age: "35",
+//   color: "white",
+//   "fav food": "beer",
+// };
+
+// for (let key in user) {
+//   console.log(key);
+//   console.log(user[key]);
+// }
+
+// let user = {
+//   name: "John",
+//   age: "35",
+// };
+
+// let clone = {};  // cloning method
+// for (let keys in user) {
+//   clone[keys] = user[keys];
+// }
+// console.log(clone);
+
+// let clone = Object.assign({}, user); // cloning method
+// console.log(clone); // cloned obj
