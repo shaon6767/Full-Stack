01a8b450ab52fc9e3334ed52fca8a0c8ae05e810
Shaon6767 <chowdhuryshaon70@gmail.com>
2026-09-15 16:21:17 +0600

    object

--- JS/objectArrayString.js
+++ JS/objectArrayString.js
@@ -49,3 +49,73 @@
 
 // let clone = Object.assign({}, user); // cloning method
 // console.log(clone); // cloned obj
+
+// there are also a method called structuredClone().This method can clone most data types, such as objects, arrays, primitive values.
+
+// function hello() {
+//   console.log(this.name);
+// }
+// hello();
+
+// let user = {
+//   name: "helo",
+// };
+
+// user.sayHelo = function () {
+//   console.log(this.name);
+// };
+
+// user.sayHelo();
+
+// function User(name) {
+//   this.name = name;
+//   this.age = 23;
+// }
+
+// let user = new User("Jack");
+
+// console.log(user.name);
+
+// let obj = new Object();
+// console.log(obj);
+
+// let arr = new Array();
+// console.log(arr);
+
+// let fn = new Function();
+// console.log(fn);
+
+// class Name {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// let obj1 = new Name("Shawn", 20);
+// console.log(obj1);
+
+// let obj2 = new Name("Mike", 26);
+// console.log(obj2);
+
+// function Obj(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// let newObj = new Obj("shawn", 26);
+// console.log(newObj);
+
+// function User() {
+//   this.name = "John";
+// }
+
+// console.log(new User().name);
+
+// function UserTwo() {
+//   this.name = "Mike";
+//   return { name: "Jake" };
+// }
+
+// console.log(new UserTwo());
+
