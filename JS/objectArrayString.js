20af690023bdaf407eec518eae5c2fcb9413d7ae
Shaon6767 <chowdhuryshaon70@gmail.com>
2026-09-16 15:29:08 +0600

    Map

--- JS/objectArrayString.js
+++ JS/objectArrayString.js
@@ -205,12 +205,97 @@
 
 // console.log(count)
 
+// let fruits = ["apple", "apple", "mango", "apple"];
 
-let fruits = ["apple", "apple", "mango", "apple"];
+// let count = fruits.reduceRight((acc, i) => {
+//   acc[i] = (acc[i] || 0) + 1;
+//   return acc;
+// }, {});
 
-let count = fruits.reduceRight((acc, i) => {
-  acc[i] = (acc[i] || 0) + 1;
-  return acc;
-}, {});
+// console.log(count)
 
-console.log(count)
+/* //iterate
+let range = {
+  from: 1,
+  to: 5,
+};
+range[Symbol.iterator] = function () {
+  return {
+    current: this.from,
+    last: this.to,
+    next() {
+      if (this.current <= this.last) {
+        return { done: false, value: this.current++ };
+      } else {
+        return { done: true };
+      }
+    },
+  };
+};
+for (let result of range) {
+  console.log(result);
+}
+*/
+
+// let recipeMap = new Map([
+//   ["cucumber", 500],
+//   ["tomatoes", 350],
+//   ["onion", 50],
+// ]);
+// for (let keys of recipeMap.keys()) {
+//   console.log(keys);
+// }
+// for (let pairs of recipeMap.entries()) {
+//   console.log(pairs);
+// }
+// for (let value of recipeMap.values()) {
+//   console.log(value);
+// }
+
+// let obj = {
+//   name: "John",
+//   age: 30,
+// };
+// let map = new Map(Object.entries(obj));
+// console.log(map);
+// console.log(obj);
+
+// let prices = Object.fromEntries([
+//   ['banana', 1],
+//   ['orange', 2],
+//   ['meat', 4]
+// ]);
+// console.log(prices)
+
+// let map = new Map();
+// map.set("shawn", 25);
+// console.log(map);
+
+// let obj = Object.fromEntries(map)
+// console.log(obj)
+
+// const user = {
+//   name: "Shawn",
+//   age: 26
+// };
+
+// let u = Object.entries(user);
+// console.log(u)
+// console.log(typeof u)
+
+//array destructuring
+// let arr = [1,2]
+// let [first,last] = arr
+// console.log(first)
+// console.log(last)
+
+//object destructuring
+// let user = {
+//   name: "Mike",
+//   age: 28,
+// };
+// let { name, age } = user;
+// console.log(name);
+// console.log(age);
+
+
